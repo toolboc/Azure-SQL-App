6fc975288b60c328cacd9481449c6ec1ec2a457a
Paul DeCarlo <toolboc@gmail.com>
2022-08-29 18:56:36 +0000

    Deploy to Azure using GH Actions

---
+++ app-database/Tables/Customer.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[Customer]
+(
+  [Id] INT NOT NULL PRIMARY KEY
+  --Add first name column
+  ,[FirstName] NVARCHAR(50) NOT NULL
+  --Add last name column
+  ,[LastName] NVARCHAR(50) NOT NULL
+)
